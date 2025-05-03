daddaf6054bf2eebdfbb3fc9a13a2a7747521988
Sam <samuelivey94@gmail.com>
2025-05-03 09:26:52 -0500

    refactor

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from stats import get_num_words
 
 def get_book_text(book_path):
 
@@ -6,6 +7,8 @@ def get_book_text(book_path):
     return text
 
 def main():
-    print(get_book_text('~/bookbot/books/frankenstein.txt'))
+    text = get_book_text("/home/sam/bookbot/books/frankenstein.txt")
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
 
 main()
---
+++ stats.py
@@ -0,0 +1,6 @@
+def get_num_words(text):
+
+    num_words = len(text.split(text))
+  
+    return num_words
+
