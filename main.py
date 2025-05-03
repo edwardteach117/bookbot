a5407913fcb4d412d1a599f8701b030daad8f83b
Sam <samuelivey94@gmail.com>
2025-05-03 08:49:45 -0500

    update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+
+def get_book_text(book_path):
+
+    with open(book_path, 'r', encoding='utf-8') as f:
+        text = f.read()
+    return text
+
+def main():
+    print(get_book_text('C:/Users/samue/Bootbot/bookbot/books/frankenstein.txt'))
+
+main()
